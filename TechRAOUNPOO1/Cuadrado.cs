b12a5455ec023ccfa247faae02576f6fb48f71a9
Edder <114453344+EdderOrtega@users.noreply.github.com>
2025-06-09 11:11:08 -0600

    feat:cambiando los nombres de botones

---
+++ TechRAOUNPOO1/Cuadrado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechRAOUNPOO1
+{
+    internal class Cuadrado: Figura
+    {
+        public Cuadrado(int xInicial, int yInicial, int ancho, int alto, int color, int relleno)
+           : base(1, xInicial, yInicial, ancho, alto, color, relleno)
+        {
+            this.TipoFigura = 1; // Tipo figura 1 para cuadrado
+            this.XInicial = xInicial;
+            this.YInicial = yInicial;
+            this.Ancho = ancho;
+            this.Alto = alto;
+            this.Color = color;
+            this.Relleno = relleno;
+        }
+    }
+}
