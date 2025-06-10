0fbeace7fc5ceefe1d4ae852b7d460938c64d60e
Edder <114453344+EdderOrtega@users.noreply.github.com>
2025-06-10 12:21:54 -0600

    feat:Diseño botones

--- TechRAOUNPOO1/Form1.cs
+++ TechRAOUNPOO1/Form1.cs
@@ -1,9 +1,16 @@
+using System.Windows.Forms;
+using System.Drawing;
 namespace TechRAOUNPOO1
 {
+
     public partial class Form1 : Form
     {
+        private Color currentColor = Color.Black;
+        private Font currentFont = new Font("Microsoft Sans Serif", 8.25F);
+
         public Form1()
         {
+
             InitializeComponent();
         }
 
@@ -26,6 +33,30 @@ namespace TechRAOUNPOO1
         private void btnColorCafe_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnSelectorDeColor_Click(object sender, EventArgs e)
+        {
+
+            colorDialog1.Color = currentColor;
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                currentColor = colorDialog1.Color;
+                btnSelectorDeColor.BackColor = currentColor;
+            }
+        }
+
+        private void btnTexto_Click(object sender, EventArgs e)
+        {
+            fontDialog1.Font = currentFont;
+
+           
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                currentFont = fontDialog1.Font;
+
+    
+            }
         }
     }
 }
